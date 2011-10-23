772aee3c1cb9cb68c7c364e68622cf0df23b80cf
Roland Warmerdam <rowno@webspirited.com>
2011-10-24 01:11:09 +1300

    Add basic script analysis functionality.
     * Add the HTML5 Boilerplate CSS (with some modifications).
     * Add JSLint (modified to use CommonJS).

--- data/panel/content-script.js
+++ data/panel/content-script.js
@@ -4,6 +4,8 @@
 
 var $scripts = $('#scripts'),
     $nav = $('#nav'),
+    $analyse = $('#analyse'),
+    $results = $('#results'),
     activePage = {};
 
 self.port.on('scripts', function (content) {
@@ -11,19 +13,63 @@ self.port.on('scripts', function (content) {
 });
 
 $scripts.delegate('input', 'change', function () {
-    self.port.emit('script-change', $(this).val(), $(this).is(':checked'));
+    var $this = $(this);
+    self.port.emit('script-change', $this.val(), $this.is(':checked'));
 });
 
 
-activePage.navItem = $nav.find('li:first-child').addClass('active');
-activePage.page = $('#' + activePage.navItem.data('id')).show();
-
-$nav.delegate('li', 'click', function () {
-    var $this = $(this);
+function changePage(pageId) {
+    var navItem = $nav.find('li[data-id=\'' + pageId + '\']');
 
     activePage.navItem.removeClass('active');
     activePage.page.hide();
 
-    activePage.navItem = $this.addClass('active');
-    activePage.page = $('#' + $this.data('id')).show();
+    activePage.navItem = navItem.addClass('active');
+    activePage.page = $('#' + pageId).show();
+}
+
+activePage.navItem = $nav.find('li:first-child').addClass('active');
+activePage.page = $('#' + activePage.navItem.data('id')).show();
+
+
+$nav.delegate('li', 'click', function () {
+    changePage($(this).data('id'));
+});
+
+
+var Analyse = {
+    start: function () {
+        $analyse.attr('disabled', 'disabled');
+        $analyse.text('Analysing');
+        changePage('results');
+        $results.html('');
+    },
+
+    stop: function () {
+        $analyse.removeAttr('disabled');
+        $analyse.text('Analyse');
+    }
+};
+
+$analyse.click(function () {
+    var scripts = [];
+
+    $scripts.find('input').each(function () {
+        var $this = $(this);
+        if ($this.is(':checked')) {
+            scripts.push($this.val());
+        }
+    });
+
+    $analyse.blur();
+    Analyse.start();
+    self.port.emit('analysis-start', scripts);
+});
+
+self.port.on('analysis-result', function (content) {
+    $results.append(content);
+});
+
+self.port.on('analysis-complete', function () {
+    Analyse.stop();
 });
